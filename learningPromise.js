83d74bc92443a58a86b8d9f31d1ff1704bdd7c00
Zain ul Hassan <108335743+Zainulhassan01@users.noreply.github.com>
2023-05-11 07:26:20 -0700

    Calling pokemon API with Promise
    Calling and Handling the API response using Promise and then logging errors if we didn't get the response of 200.

--- learningPromise.js
+++ learningPromise.js
@@ -10,3 +10,27 @@ promise.then(
 promise.catch(
     (error) => {console.log(error)}
 )
+//// Calling an API with promise //////
+
+const ALL_POKEMONS_URL = 'https://pokeapi.co/api/v2/pokemon?limit=50';
+
+customPromise = (URL = ALL_POKEMONS_URL) => {
+    let promise = new Promise(function(resolve, reject){
+        let req = new XMLHttpRequest()
+        req.open("GET", URL)
+        req.onload = function () {
+            if (req.status == 200) {
+                resolve(req.response)
+            } else {
+                reject("There is an Error!")
+            }
+        }
+        req.send()
+    })
+    return promise
+}
+
+customPromise.then(
+    (result) => console.log(result),
+    (error) => (console.error('Something went wrong', error))
+)
